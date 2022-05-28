b16c890448a2ceeb24b0f47491f952d3bc235959
HoangTien339 <tiennh21@fpt.edu.vn>
2022-05-28 09:09:35 +0700

    JpaRepository: Create/Delete

--- src/main/java/su22/sof3021/controllers/admin/AccountController.java
+++ src/main/java/su22/sof3021/controllers/admin/AccountController.java
@@ -1,16 +1,23 @@
 package su22.sof3021.controllers.admin;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import su22.sof3021.beans.AccountModel;
+import su22.sof3021.entities.Account;
+import su22.sof3021.repositories.AccountRepository;
 
 @Controller
 @RequestMapping("/admin/accounts")
 public class AccountController {
+	@Autowired
+	private AccountRepository accountRepo;
+	
 	@GetMapping("/create")
 	public String create(@ModelAttribute("account") AccountModel acc)
 	{
@@ -18,13 +25,29 @@ public class AccountController {
 	}
 	
 	@PostMapping("/store")
-	public String store(AccountModel accModel)
+	public String store(AccountModel model)
 	{
+		Account acc = new Account();
+		
+		acc.setFullname(model.getFullname());
+		acc.setUsername(model.getUsername());
+		acc.setEmail(model.getEmail());
+		acc.setPassword(model.getPassword());
+		acc.setPhoto(model.getPhoto());
+		acc.setAdmin(model.getAdmin());
+		acc.setActivated(0);
+		
+		this.accountRepo.save(acc);
+		
 		return "redirect:/admin/accounts/create";
 	}
 	
-	public String delete()
+	@GetMapping("delete/{id}")
+	public String delete(@PathVariable("id") Account account)
 	{
+		System.out.println(account.getEmail());
+		// Account a = this.accountRepo.getById(id);
+
 		return "redirect:/admin/accounts/create";
 	}
 }
